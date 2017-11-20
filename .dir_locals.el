98aed47dd3509f5e5973aa9944025d4e0f7e777a
Benjamin Andre <andre@ucar.edu>
2017-11-20 16:37:06 -0700

    Add .dir_locals.el for consistent emacs indenting.

---
+++ .dir_locals.el
@@ -0,0 +1,12 @@
+; -*- mode: Lisp -*-
+
+((python-mode
+  . (
+     ;; fill the paragraph to 80 columns when using M-q
+     (fill-column . 80)
+
+     ;; Use 4 spaces to indent in Python
+     (python-indent-offset . 4)
+     (indent-tabs-mode . nil)
+     )))
+
