be85b7d1bd32363ddf31cca7470c68a716add1a8
Jim Edwards <jedwards@ucar.edu>
2020-05-18 12:19:20 -0600

    fix the test

--- test/test_sys_checkout.py
+++ test/test_sys_checkout.py
@@ -1361,14 +1361,13 @@ class TestSysCheckout(BaseTestSysCheckout):
         # create the top level externals file
         self._generator.container_full(under_test_dir)
 
-        # inital checkout, first try a nonexistant component argument noref
-        checkout_args = ['--exclude','simp_opt']
+        # inital checkout, exclude simp_opt
+        checkout_args = ['--exclude','simp_opt', '--logging']
         checkout_args.extend(self.checkout_args)
-        save_checkout_args = self.checkout_args
-        self.checkout_args = checkout_args
+        overall, tree = self.execute_cmd_in_dir(under_test_dir, checkout_args)
+        checkout_args.append("--status")
         overall, tree = self.execute_cmd_in_dir(under_test_dir, checkout_args)
         self._check_container_component_post_checkout3(overall, tree)
-        self.checkout_args = save_checkout_args
 
     def test_mixed_simple(self):
         """Verify that a mixed use repo can serve as a 'full' container,
