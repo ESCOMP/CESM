cd186279e056d17fbf0b091620393674558ec66e
Bill Sacks <sacks@ucar.edu>
2020-09-25 16:55:37 -0600

    Change label for latest dev code
    In talking with Gokhan and Mariana, I realized that "latest development
    code" could be confused with our terminology for "development
    releases". We wanted to make it more clear that this version of the
    documentation is for unreleased code.

--- versions/master/html/_static/documentation_options.js
+++ versions/master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Latest Development Code',
+    VERSION: 'Upcoming/Beta Version',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
